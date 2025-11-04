43d29cd2e42da82410d5736ce657d58b0dba7082
FlavioFj18 <fuxiflavio86@gmail.com>
2025-11-04 11:58:17 +0100

    Storing tasks
    Configuring the localStorage as the location to store the application tasks

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,22 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import AddTask from "./components/AddTask";
 import Tasks from "./components/Tasks";
-import {v4} from "uuid";
+import { v4 } from "uuid";
 
 function App() {
-  const [task, setTasks] = useState([
-    {
-      id: 1,
-      title: "Estudar programacao",
-      description:
-        "Estudar programacao para se tornar um desenvolvedor full stack.",
-      isComplited: false,
-    },
-    {
-      id: 2,
-      title: "Estudar ingles",
-      description:
-        "Estudar ingles para se tornar um desenvolvedor full stack com vantagem no mercado",
-      isComplited: false,
-    },
-    {
-      id: 3,
-      title: "Estudar programacao",
-      description:
-        "Estudar programacao para se tornar um desenvolvedor full stack.",
-      isComplited: false,
-    },
-  ]);
+  const [task, setTasks] = useState(
+    JSON.parse(localStorage.getItem("task")) || []
+  );
+
+  useEffect(() => {
+    localStorage.setItem("task", JSON.stringify(task));
+  }, [task]);
 
   function onTaskClick(taskId) {
     const newTasks = task.map((task) => {
       //Preciso actualizar a task
       if (task.id === taskId) {
-        return { ...task, isCompleted: !task.isCompleted};
+        return { ...task, isCompleted: !task.isCompleted };
       }
       //Nao preciso actualizar a task
       return task;
@@ -53,7 +37,7 @@ function App() {
       isCompleted: false,
     };
     setTasks([...task, newTask]);
-  } 
+  }
 
   return (
     <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
@@ -61,8 +45,12 @@ function App() {
         <h1 className="text-3xl text-slate-100 font-bold text-center">
           Gerenciador de Tarefas
         </h1>
-        <AddTask onTaskAddSubmit={onTaskAddSubmit}/>
-        <Tasks tasks={task} onTaskClick={onTaskClick} onTaskClickDelete={onTaskClickDelete}/>
+        <AddTask onTaskAddSubmit={onTaskAddSubmit} />
+        <Tasks
+          tasks={task}
+          onTaskClick={onTaskClick}
+          onTaskClickDelete={onTaskClickDelete}
+        />
       </div>
     </div>
   );
